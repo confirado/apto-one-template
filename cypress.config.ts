7be3f1693f94d31ae32b91a99c51aa0be564b86c
ronnybaumann <ronnybaumann@users.noreply.github.com>
2023-10-12 13:18:54 +0200

    APTOONE-237: Cypress in Struktur von Apto.ONE integrieren

---
+++ cypress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "cypress";
+
+let path = 'vendor/apto-one/core/Tests/';
+if (process.env['APTO_CYPRESS_ENV'] === 'dev') {
+  path = 'src/apto-one-core/Tests/';
+}
+
+const folders = {
+  specPattern: path + '/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  downloadsFolder: path + '/cypress/Tests/_downloads',
+  fixturesFolder: path + '/cypress/_fixtures',
+  supportFolder: path + '/cypress/_support',
+  videosFolder: path + '/cypress/_videos',
+  screenshotsFolder: path + '/cypress/_screenshots'
+};
+
+export default defineConfig({
+  e2e: {
+    ...folders,
+    env: {
+      baseUrl: 'http://grobi.projektversion.de/apto-one-template/web/#'
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
