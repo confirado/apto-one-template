672f404111fba76344c461a3dc620b9c736c09a1
Arto Avagyan <arto.avagyan80@gmail.com>
2024-01-31 11:34:16 +0400

    tests/apto-one-core massive refactoring of code locations

--- apps/frontend/src/configs-static/screen-sizes.ts
+++ apps/frontend/src/configs-static/screen-sizes.ts
@@ -3,3 +3,10 @@ export enum ScreenSizes {
   tablet = 1024,
   desktop = 1279,
 }
+
+// use for local development testing only!!!
+// export enum ScreenSizes {
+//   mobile = 768,
+//   tablet = 768,
+//   desktop = 768,
+// }
