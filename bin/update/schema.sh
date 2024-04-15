dca7d3c0ce9dc2a921674266c4a3e373d10947bc
ronnybaumann <ronnybaumann@users.noreply.github.com>
2024-04-15 17:06:57 +0200

    add schema update script

---
+++ bin/update/schema.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# change directory to project root
+SCRIPT_DIRECTORY="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+cd ${SCRIPT_DIRECTORY}
+cd ../..
+
+# update database
+php bin/console doctrine:schema:validate
+php bin/console doctrine:schema:update --dump-sql
+
+echo "Do you want to execute the above db queries? (Y/n)"
+read EXECUTE_DB_QUERIES
+
+if [ ${EXECUTE_DB_QUERIES} = "Y" ];
+then
+  php bin/console doctrine:schema:update --force
+fi
